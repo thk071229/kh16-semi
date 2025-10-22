4a8a3f3942bdf593d59205b0c189d19e423a28dd
jaeyeah <limjh819@naver.com>
2025-10-22 17:45:18 +0900

    eventListVO 생성

---
+++ semi/src/main/java/com/kh/semi/mapper/EventListMapper.java
@@ -0,0 +1,32 @@
+package com.kh.semi.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.kh.semi.dto.EventDto;
+import com.kh.semi.vo.EventListVO;
+
+@Component
+public class EventListMapper implements RowMapper<EventListVO>{
+
+	@Override
+	public EventListVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		EventListVO eventListVO = new EventListVO();
+		eventListVO.setEventNo(rs.getInt("event_no"));
+		eventListVO.setEventClub(rs.getInt("event_club"));
+		eventListVO.setEventWriter(rs.getString("event_writer"));
+		eventListVO.setEventTitle(rs.getString("event_title"));
+		eventListVO.setEventMaxPeople(rs.getInt("event_max_people"));
+		eventListVO.setEventAddress(rs.getString("event_address"));
+		eventListVO.setEventDate(rs.getTimestamp("event_date"));
+		eventListVO.setClubName(rs.getString("club_name"));
+		return eventListVO;
+	}
+
+
+
+}
---
+++ semi/src/main/java/com/kh/semi/vo/EventListVO.java
@@ -0,0 +1,24 @@
+package com.kh.semi.vo;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class EventListVO {
+
+	private int eventNo;
+	private int eventClub;
+	private String eventWriter;
+	private String eventTitle;
+	private int eventMaxPeople;
+	private String eventAddress;
+	private Timestamp eventDate;
+	private String clubName;
+	
+	
+	
+}
