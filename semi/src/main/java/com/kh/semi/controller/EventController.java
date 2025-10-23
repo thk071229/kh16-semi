63fefa0b3caf2d20f0c89fb26c5114f51e39650e
jaeyeah <limjh819@naver.com>
2025-10-23 14:27:10 +0900

    Event - 이미지 첨부파일 구현

--- semi/src/main/java/com/kh/semi/controller/EventController.java
+++ semi/src/main/java/com/kh/semi/controller/EventController.java
@@ -1,7 +1,13 @@
 package com.kh.semi.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.kh.semi.dao.EventDao;
 import com.kh.semi.dto.EventDto;
 import com.kh.semi.error.TargetNotFoundException;
+import com.kh.semi.service.AttachmentService;
 import com.kh.semi.vo.EventListVO;
 
 import jakarta.servlet.http.HttpSession;
@@ -25,6 +32,9 @@ public class EventController {
 	@Autowired
 	private EventDao eventDao;
 
+	@Autowired
+	private AttachmentService attachmentService;
+	
 	// 등록
 	@GetMapping("/add")
 	public String add(Model model, @RequestParam int clubNo) {
@@ -92,10 +102,41 @@ public class EventController {
 	
 	@PostMapping("/edit")
 	public String edit(@ModelAttribute EventDto eventDto) {
-		EventDto findDto = eventDao.selectOne(eventDto.getEventNo());
-		if(findDto==null) throw new TargetNotFoundException("존재하지 않는 이벤트");
+		EventDto beforeDto = eventDao.selectOne(eventDto.getEventNo());
+		if(beforeDto==null) throw new TargetNotFoundException("존재하지 않는 이벤트");
+		
+		// 수정하면서 이미지를 삭제하면, attachment DB에 삭제 정보를 반영
+		//기존 글과 변경될 글의 이미지 번호 차이를 구하기 위한 코드
+		// - 기존 글의 번호추출 -> 저장
+		Set<Integer> before = new HashSet<>();
+		Document beforeDocument = Jsoup.parse(beforeDto.getEventContent());
+		Elements beforeElements = beforeDocument.select(".custom-image");
+		for(Element element : beforeElements) { // 하나씩 반복하며
+			int attachmentNo = Integer.parseInt(element.attr("data-pk"));
+			before.add(attachmentNo);
+		}
+	
+		// - 수정된 글의 번호추출 -> 저장
+		Set<Integer> after = new HashSet<>();
+		Document afterDocument = Jsoup.parse(eventDto.getEventContent());
+		Elements afterElements = afterDocument.select(".custom-image");
+		for(Element element : afterElements) { // 하나씩 반복하며
+			int attachmentNo = Integer.parseInt(element.attr("data-pk"));
+			after.add(attachmentNo);
+		}
+		
+		// before에만 있는 이미지 번호를 구하자! (차집합)
+		// -  before에서 after를 뺌
+		Set<Integer> minus = new HashSet<>(); 
+		minus.addAll(before); //before를 넣어놓고
+		minus.removeAll(after); // after에 있는 내용을 제거
+		//minus에 들어있는 번호가 기존에 잇었지만 사라진 이미지 번호를 제거
+		for(int attachmentNo : minus) {
+			attachmentService.delete(attachmentNo);
+		}
+		
 		eventDao.update(eventDto);
-		return "redirect:detail?eventNo="+findDto.getEventNo();
+		return "redirect:detail?eventNo="+eventDto.getEventNo();
 	}
 	
 	// 삭제
@@ -103,6 +144,14 @@ public class EventController {
 	public String delete(@RequestParam int eventNo) {
 		EventDto eventDto = eventDao.selectOne(eventNo);
 		if(eventDto==null) throw new TargetNotFoundException("존재하지 않는 이벤트번호");
+		// 글 본문에 포함된 모든 <img>를 찾아서 해당하는 이미지의 글번호를 삭제
+				// - summernote가 만든 html 형식의 글에서 원하는 항목을 탐색 (Jsoup 사용)
+			Document document = Jsoup.parse(eventDto.getEventContent()); // HTML로 해석해서
+			Elements elements = document.select(".custom-image"); // <img>를 찾고
+			for(Element element : elements) { // 하나씩 반복하며
+				int attachmentNo = Integer.parseInt(element.attr("data-pk"));
+				attachmentService.delete(attachmentNo);
+			}
 		eventDao.delete(eventNo);
 		return "redirect:list?clubNo="+eventDto.getEventClub();
 	}
--- semi/src/main/java/com/kh/semi/restcontroller/EventRestController.java
+++ semi/src/main/java/com/kh/semi/restcontroller/EventRestController.java
@@ -1,13 +1,19 @@
 package com.kh.semi.restcontroller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.semi.dao.EventAttendeeDao;
+import com.kh.semi.error.TargetNotFoundException;
+import com.kh.semi.service.AttachmentService;
 import com.kh.semi.vo.EventAttendeeVO;
 
 import jakarta.servlet.http.HttpSession;
@@ -19,6 +25,9 @@ public class EventRestController {
 	
 	@Autowired
 	private EventAttendeeDao eventAttendeeDao;
+	
+	@Autowired
+	private AttachmentService attachmentService;
 	
 	//참여자 확인
 	@GetMapping("/check")
@@ -34,7 +43,7 @@ public class EventRestController {
 	}
 	
 	
-	// 좋아요 설정
+	// 참여/불참 설정
 	@GetMapping("/action")
 	public EventAttendeeVO action(HttpSession session, @RequestParam int eventNo) {
 		String loginId = (String)session.getAttribute("loginId");
@@ -44,13 +53,21 @@ public class EventRestController {
 			eventAttendeeDao.delete(loginId,eventNo);
 			eventAttendeeVO.setAttend(false);
 		}
-		else { // 좋아요를 누를 이력이 없으면
+		else { // 참여 기록 확인한 뒤
 			eventAttendeeDao.insert(loginId, eventNo);
 			eventAttendeeVO.setAttend(true);
 		}
 		int count = eventAttendeeDao.countByEventNo(eventNo);
 		eventAttendeeVO.setCount(count);
 		return eventAttendeeVO;
-
 	}
+	
+	// 임시 이미지 업로드
+	@PostMapping("/temp")
+	public int temp(@RequestParam MultipartFile attach) throws IllegalStateException, IOException {
+		if(attach.isEmpty()) throw new TargetNotFoundException("파일이 업로드 되지 않았습니다");
+		return attachmentService.save(attach);
+	}
+	
+
 }
