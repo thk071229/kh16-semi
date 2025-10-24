4942afa0354660dbefdb57cecced4a18e458a0af
hansoobin-11 <vcoart@naver.com>
2025-10-24 17:53:19 +0900

    interceptor에서 club/add 삭제

--- semi/src/main/java/com/kh/semi/restcontroller/BoardRestController.java
+++ semi/src/main/java/com/kh/semi/restcontroller/BoardRestController.java
@@ -1,8 +1,5 @@
 package com.kh.semi.restcontroller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,12 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.semi.dao.BoardDao;
 import com.kh.semi.dao.BoardLikeDao;
-import com.kh.semi.error.TargetNotFoundException;
-import com.kh.semi.service.AttachmentService;
 import com.kh.semi.vo.BoardLikeVO;
 
 import jakarta.servlet.http.HttpSession;
