943a37e8c78642f5e9d378d4487bf5ae17e81f71
siru129 <sirusiru1209@gmail.com>
2025-10-29 11:12:27 +0900

    메인 홈에있는 검색창 구현 완료

--- semi/src/main/java/com/kh/semi/restcontroller/RegionRestCotroller.java
+++ semi/src/main/java/com/kh/semi/restcontroller/RegionRestCotroller.java
@@ -1,5 +1,21 @@
 package com.kh.semi.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.semi.dao.RegionDao;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/rest/region")
 public class RegionRestCotroller {
 
+	@Autowired
+	private RegionDao regionDao;
+	
+	/*
+	 * @GetMapping("/list") public
+	 */
 }
