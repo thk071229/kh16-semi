31d8e0c41fe60ac379b24776fa16c539df9ce733
jaeyeah <limjh819@naver.com>
2025-10-29 17:22:16 +0900

    포인트 사용처에 따른 memberActive 계산 수정

--- semi/src/main/java/com/kh/semi/vo/MemberActiveVO.java
+++ semi/src/main/java/com/kh/semi/vo/MemberActiveVO.java
@@ -12,12 +12,16 @@ public class MemberActiveVO {
 	private String memberNickname;
 	private int memberEventAttend;
 	private int memberBoardWrite;
+	private int memberPointUse;
 	
 	int eventImportance = 20; // 정모 참여시 가중치
 	int boardImportance = 5; // 게시글 작성시 가중치
+	int PointUseImportance = -500; // 생성권 구매 가중치
 	
 	public int memberPoint() {
-		int resultPoint = memberEventAttend*eventImportance+ memberBoardWrite*boardImportance;
+		int resultPoint = memberEventAttend*eventImportance
+							+ memberBoardWrite*boardImportance
+							+ memberPointUse*PointUseImportance;
 		return resultPoint;
 	}
 										
