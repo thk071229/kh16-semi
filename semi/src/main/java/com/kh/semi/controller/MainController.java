633e5639e6e48ad13d3755e220a25a9bc1346b86
jaeyeah <limjh819@naver.com>
2025-10-31 18:10:14 +0900

    메인페이지 VO 파라미터 수정

--- semi/src/main/java/com/kh/semi/controller/MainController.java
+++ semi/src/main/java/com/kh/semi/controller/MainController.java
@@ -115,7 +115,7 @@ public class MainController {
 
 		/// 좋아요순용 PageVO 설정
 		PageVO likePageVO = new PageVO();
-		likePageVO.setPage(boardPage);
+		likePageVO.setPage(likePage);
 		likePageVO.setRegionDepth1(regionDepth1);
 		likePageVO.setRegionDepth2(regionDepth2);
 		likePageVO.setDataCount(countDao.clubLikeListCount(likePageVO));
